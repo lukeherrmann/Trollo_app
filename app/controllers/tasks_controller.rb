dd9d17cd81a07039437f6d731e64bb7090afa02f
Luke Herrmann <herrmannluke@gmail.com>
2019-03-03 16:58:08 -0700

    CRUD actions for Board and List Models

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_list
+
   def index
+    @tasks = Task.new
   end
 
   def show
@@ -9,5 +12,31 @@ class TasksController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    @task = @list.tasks.new(task_params)
+
+    if @task.save
+      redirect_to board_list_path(@list.board_id, @list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to board_list_path(@list.board_id, @list)
+    red
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:task_name)
   end
 end
